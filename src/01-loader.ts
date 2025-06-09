20c45ce7508ee0c45863541f55993ced95833f08
Alex Naupay <alexnaupay@gmail.com>
2025-06-08 21:27:56 -0500

    Feat: Add text splitting functionality using LangChain
    - Introduced `06-text-spliter.ts` to split text and documents using `RecursiveCharacterTextSplitter`.
    - Updated `package.json` to include `langchain` dependency.
    - Commented out the default `loadPdf` invocation in `01-loader.ts` for cleaner integration.

--- src/01-loader.ts
+++ src/01-loader.ts
@@ -8,10 +8,10 @@ export async function loadPdf(pdfPath?: string) {
     return await pdfLoader.load()
 }
 
-loadPdf().then(docs => {
+/*loadPdf().then(docs => {
     console.log('Docs Length: ', docs.length)
     console.log('Docs: ', docs)
     console.log('Docs Metadata: ', docs[0].metadata)
     console.log('--------------------------------------')
     console.log('Doc pageContent: ', docs[0].pageContent)
-});
+});*/
---
+++ src/06-text-spliter.ts
@@ -0,0 +1,32 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import {loadPdf} from "./01-loader";
+
+const text = `Hi.\n\nI'm Harrison.\n\nHow? Are? You?\nOkay then f f f f.
+This is a weird text to write, but gotta test the splittingggg some how.\n\n
+Bye!\n\n-H.`;
+
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 10,
+    chunkOverlap: 2,
+    //lengthFunction: (text) => text.length,
+})
+
+// split text
+splitter.splitText(text).then(console.log).catch(console.error)
+
+// create Documents from texts
+splitter.createDocuments([text]).then(console.info).catch(console.error)
+
+const splitter2 = new RecursiveCharacterTextSplitter({
+    chunkSize: 1200,
+    chunkOverlap: 180,  // 15%
+    //lengthFunction: (text) => text.length,
+})
+
+// split documents
+loadPdf().then(async (docs)=> {
+    const chunks = await splitter2.splitDocuments(docs)
+    //console.log(chunks[10].metadata)
+    console.log(chunks)
+    console.log('Chunk sizes: ',chunks.length)
+})
